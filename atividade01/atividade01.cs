93881d15f6431ea10a9626b7f2e9519def5cb331
leidsontomaz <77466837+leidsontomaz@users.noreply.github.com>
2021-01-14 19:21:45 -0300

    Create atividade01.cs

---
+++ atividade01/atividade01.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int a,b;
+    a = int.Parse(Console.ReadLine());
+    b= int.Parse(Console.ReadLine());
+
+    Console.WriteLine(a/b:0.00);
+
+  }
+}
